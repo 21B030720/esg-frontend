f54a0a15725324452103814976c2da43cc47f0a1
chronosgit <warfacemeister@mail.ru>
2024-07-20 09:49:35 +0500

    fix: fixed posting apply form

--- src/common/api/postApplication.js
+++ src/common/api/postApplication.js
@@ -1,7 +1,7 @@
-import { BACKEND_ADDRESS } from "@common/baseUrls";
-import fetchWrapper from "./fetchWrapper";
+import { BACKEND_ADDRESS } from '@common/baseUrls';
+import fetchWrapper from './fetchWrapper';
 
-const postApplication = async(form) => {
+const postApplication = async form => {
 	const formData = new FormData();
 
 	formData.append('name', form['name']);
@@ -10,29 +10,29 @@ const postApplication = async(form) => {
 	formData.append('directionID', form['directionID']);
 
 	for (let i = 0; i < form['projectFile'].length; i++) {
-    formData.append('projectFile[]', form['projectFile'][i]);
-  }
-  
-	// formData.forEach(function(value, key) {
-	// 	console.log("FORM DATA:", key, value);
-	// });
+		formData.append('projectFile[]', form['projectFile'][i]);
+	}
 
 	const url = `${BACKEND_ADDRESS}/applications`;
 	const option = {
 		method: 'POST',
-		redirect: "follow",
+		redirect: 'follow',
 		headers: {
-			"Authorization": `Bearer ${localStorage.getItem('token')}`,
+			Authorization: `Bearer ${localStorage.getItem('token')}`,
 		},
 		body: formData,
 	};
-	
+
 	try {
-		const { data, success, error } = await fetchWrapper(url, option, 'postApplication');
-		
-		return {data, success, error};
-	} catch(error) {
-		console.error("Error in postApplication: ", error);
+		const { data, success, error } = await fetchWrapper(
+			url,
+			option,
+			'postApplication'
+		);
+
+		return { data, success, error };
+	} catch (error) {
+		console.error('Error in postApplication: ', error);
 	}
 };
 
--- src/modules/projects/hooks/useProjects.jsx
+++ src/modules/projects/hooks/useProjects.jsx
@@ -1,9 +1,8 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { BACKEND_ADDRESS } from "@common/baseUrls";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { BACKEND_ADDRESS } from '@common/baseUrls';
 
 const useProjects = () => {
-
 	const [projects, setProjects] = useState([]);
 	const [isError, setError] = useState(null);
 	const [isLoading, setLoading] = useState(false);
@@ -13,18 +12,22 @@ const useProjects = () => {
 		setLoading(true);
 
 		const fetchProjects = async () => {
-			axios.get(`${BACKEND_ADDRESS}/projects`, {
-				headers: { 'Content-Type': 'application/json', }
-			})
-			.then(response => {
-				setProjects(response.data);
-				console.log(response);
-				setLoading(false);
-			})
-			.catch(error => {
-				setError(error);
-				setLoading(false);
-			})
+			axios
+				.get(`${BACKEND_ADDRESS}/projects`, {
+					headers: {
+						'Content-Type': 'application/json',
+						Authorization: `Bearer ${localStorage.getItem('token')}`,
+					},
+				})
+				.then(response => {
+					setProjects(response.data);
+					console.log(response);
+					setLoading(false);
+				})
+				.catch(error => {
+					setError(error);
+					setLoading(false);
+				});
 		};
 
 		fetchProjects();
