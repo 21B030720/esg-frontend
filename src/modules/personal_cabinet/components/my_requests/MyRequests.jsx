3300e8012b8d5788c7fcc638208c07f219563238
chronosgit <warfacemeister@mail.ru>
2024-07-20 08:49:11 +0500

    style: prettified

--- src/modules/personal_cabinet/components/my_requests/MyRequests.jsx
+++ src/modules/personal_cabinet/components/my_requests/MyRequests.jsx
@@ -4,62 +4,61 @@ import titleIcon from '@assets/icons/title.png';
 import statusIcon from '@assets/icons/status.png';
 import styles from './my_requests.module.css';
 
-const MyRequests = ({requests}) => {
-  // const requests = fakeRequests;
+const MyRequests = ({ requests }) => {
+	// const requests = fakeRequests;
 
-	if(requests == null) {
+	if (requests == null) {
 		return;
 	}
 
-	if(requests.length === 0) {
-		return <p>Заявок нет</p>
+	if (requests.length === 0) {
+		return <p>Заявок нет</p>;
 	}
 
-  const getStatusStyle = (status) => {
-    switch (status) {
-      case 'Отказано':
-        return styles.denied;
-      case 'В обработке':
-        return styles.inProgress;
-      case 'Опубликовано':
-        return styles.published;
-      default:
-        return '';
-    }
-  };    
-    
-  return (
-      <div className={styles.dataTable}>
+	const getStatusStyle = status => {
+		switch (status) {
+			case 'Отказано':
+				return styles.denied;
+			case 'В обработке':
+				return styles.inProgress;
+			case 'Опубликовано':
+				return styles.published;
+			default:
+				return '';
+		}
+	};
 
-      <div className={`${styles.row} ${styles.header}`}>
-        <div className={styles.cell}>
-          <img src={dateIcon} alt="Date" className={styles.icon} /> Дата
-        </div>
+	return (
+		<div className={styles.dataTable}>
+			<div className={`${styles.row} ${styles.header}`}>
+				<div className={styles.cell}>
+					<img src={dateIcon} alt="Date" className={styles.icon} /> Дата
+				</div>
 
-        <div className={styles.cell}>
-          <img src={titleIcon} alt="Title" className={styles.icon} /> Название
-        </div>
+				<div className={styles.cell}>
+					<img src={titleIcon} alt="Title" className={styles.icon} /> Название
+				</div>
 
-        <div className={styles.cell}>
-          <img src={statusIcon} alt="Status" className={styles.icon} /> Статус
-        </div>
-      </div>
+				<div className={styles.cell}>
+					<img src={statusIcon} alt="Status" className={styles.icon} /> Статус
+				</div>
+			</div>
 
-      {
-        requests.map(request => (
-          <div className={styles.row} key={request.id}>
-            <div className={styles.cell}>{request.date}</div>
+			{requests.map(request => (
+				<div className={styles.row} key={request.id}>
+					<div className={styles.cell}>{request.date}</div>
 
-            <div className={styles.cell}>{request.title}</div>
+					<div className={styles.cell}>{request.title}</div>
 
-            <div className={styles.cell}>
-              <div className={getStatusStyle(request.status)}>{request.status}</div>
-            </div>
-          </div>
-        ))
-      }
-    </div>
-  );
+					<div className={styles.cell}>
+						<div className={getStatusStyle(request.status)}>
+							{request.status}
+						</div>
+					</div>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default MyRequests;
