4266402e7005a1213a0f1a971c05ab8394f50939
chronosgit <warfacemeister@mail.ru>
2024-07-24 00:22:52 +0500

    style: prettied

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import routes from "@routes/routes";
-import { useTranslation } from "react-i18next";
-import { registerLocale, setDefaultLocale } from "react-datepicker";
-import ru from "date-fns/locale/ru";
-import kk from "date-fns/locale/kk";
-import enGB from "date-fns/locale/en-GB";
-import { AuthProvider } from "./contexts/AuthContext";
-
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import routes from '@routes/routes';
+import { useTranslation } from 'react-i18next';
+import { registerLocale, setDefaultLocale } from 'react-datepicker';
+import ru from 'date-fns/locale/ru';
+import kk from 'date-fns/locale/kk';
+import enGB from 'date-fns/locale/en-GB';
+import { AuthProvider } from './contexts/AuthContext';
 
 const App = () => {
+	const { i18n } = useTranslation();
 
-  const { i18n } = useTranslation();
+	registerLocale('ru', ru);
+	registerLocale('en', enGB);
+	registerLocale('kk', kk);
 
-  registerLocale("ru", ru);
-  registerLocale("en", enGB);
-  registerLocale("kk", kk);
+	setDefaultLocale(i18n.language);
 
-  setDefaultLocale(i18n.language);
+	const router = createBrowserRouter(routes);
 
-  const router = createBrowserRouter(routes);
-
-  return (
-    <AuthProvider>
+	return (
+		<AuthProvider>
 			<RouterProvider router={router} />
 		</AuthProvider>
-  );
+	);
 };
 
 export default App;
